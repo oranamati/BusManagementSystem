345b8271924a19c3353cac0ab28aaaacf1585761
oranamati <73428822+oranamati@users.noreply.github.com>
2024-08-19 19:13:56 +0300

    Update BusLines.cs

--- dotNet5781_02_7776/BusLines.cs
+++ dotNet5781_02_7776/BusLines.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
-namespace dotNet5781_02_0406_3977
+namespace dotNet5781_02_7776
 {
     public class BusLines : IEnumerable
     {
